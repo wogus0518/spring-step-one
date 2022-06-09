9491015637ce9bdf21d593321f173b54e0516392
JaeHyun <wogus0518@naver.com>
2022-06-09 17:00:42 +0900

    새로운 할인 정책 개발

---
+++ src/test/java/hello/core/discount/RateDiscountPolicyTest.java
@@ -0,0 +1,38 @@
+package hello.core.discount;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+
+class RateDiscountPolicyTest {
+    RateDiscountPolicy discountPolicy = new RateDiscountPolicy();
+
+    @Test
+    @DisplayName("VIP는 10% 할인이 적용되어야 한다")
+    void vip_o() {
+        //given
+        Member member = new Member(1L, "memberA", Grade.VIP);
+
+        //when
+        int discount = discountPolicy.discount(member, 10000);
+
+        //then
+        Assertions.assertThat(discount).isEqualTo(1000);
+    }
+
+    @Test
+    @DisplayName("BASIC은 10% 할인이 적용되면 안된다.")
+    void basic_x() {
+        //given
+        Member member = new Member(1L, "memberA", Grade.BASIC);
+
+        //when
+        int discount = discountPolicy.discount(member, 10000);
+
+        //then
+        Assertions.assertThat(discount).isEqualTo(0);
+    }
+}
