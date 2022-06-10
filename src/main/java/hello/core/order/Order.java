9bd5600d73db44e8a6e933645c927394d29fa9fa
JaeHyun <wogus0518@naver.com>
2022-06-10 12:45:08 +0900

    AppConfig Refactoring

--- src/main/java/hello/core/order/Order.java
+++ src/main/java/hello/core/order/Order.java
@@ -4,7 +4,7 @@ public class Order {
     private Long memberId;
     private String itemName;
     private int itemPrice;
-    private int discountPrice;
+    private static int discountPrice;
 
     public Order(Long memberId, String itemName, int itemPrice, int discountPrice) {
         this.memberId = memberId;
--- src/test/java/hello/core/discount/RateDiscountPolicyTest.java
+++ src/test/java/hello/core/discount/RateDiscountPolicyTest.java
@@ -1,26 +1,39 @@
 package hello.core.discount;
 
+import hello.core.AppConfig;
 import hello.core.member.Grade;
 import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.order.Order;
+import hello.core.order.OrderService;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 
 class RateDiscountPolicyTest {
-    RateDiscountPolicy discountPolicy = new RateDiscountPolicy();
+    OrderService orderService;
+    MemberService memberService;
 
+    @BeforeEach
+    void beforeEach() {
+        AppConfig appConfig = new AppConfig();
+        orderService = appConfig.orderService();
+        memberService = appConfig.memberService();
+    }
     @Test
     @DisplayName("VIP는 10% 할인이 적용되어야 한다")
     void vip_o() {
         //given
         Member member = new Member(1L, "memberA", Grade.VIP);
-
+        memberService.join(member);
         //when
-        int discount = discountPolicy.discount(member, 10000);
+        Order order = orderService.createOrder(1L, "itemA", 200000);
 
         //then
-        Assertions.assertThat(discount).isEqualTo(1000);
+        int discountPrice = order.getDiscountPrice();
+        Assertions.assertThat(discountPrice).isEqualTo(20000);
     }
 
     @Test
@@ -28,11 +41,12 @@ class RateDiscountPolicyTest {
     void basic_x() {
         //given
         Member member = new Member(1L, "memberA", Grade.BASIC);
-
+        memberService.join(member);
         //when
-        int discount = discountPolicy.discount(member, 10000);
+        Order order = orderService.createOrder(1L, "itemA", 200000);
 
         //then
-        Assertions.assertThat(discount).isEqualTo(0);
+        int discountPrice = order.getDiscountPrice();
+        Assertions.assertThat(discountPrice).isEqualTo(0);
     }
 }
