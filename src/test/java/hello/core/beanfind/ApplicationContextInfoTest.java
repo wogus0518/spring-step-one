c82e9741f3407ef7af1873f199b1d8f47d12cafb
JaeHyun <wogus0518@naver.com>
2022-06-15 16:54:01 +0900

    ComponentScan

--- src/test/java/hello/core/beanfind/ApplicationContextInfoTest.java
+++ src/test/java/hello/core/beanfind/ApplicationContextInfoTest.java
@@ -1,13 +1,13 @@
 package hello.core.beanfind;
 
-import hello.core.AppConfig;
+import hello.core.AutoAppConfig;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class ApplicationContextInfoTest {
-    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AutoAppConfig.class);
 
     @Test
     @DisplayName("모든 빈 출력하기")
